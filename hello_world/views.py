339ca25a750004910ab95e0c9f9d123eb4990df5
Sam Green <sam.green7777@yahoo.com>
2024-09-03 19:49:31 +0000

    take it off extra space in url patterns

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+"""def index(request):
     if request.method == "POST":
         return HttpResponse("You must have POSTed something")
     else:
-        return HttpResponse(request.method)
+        return HttpResponse(request.method)"""
+
+
+def index(request):
+     return HttpResponse("You must have POSTed something")
